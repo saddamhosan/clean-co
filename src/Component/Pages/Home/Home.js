9a9f084c4d2f4e6b415448f72ce8eb747acdf841
saddamhosan <contact.saddamhosan.com>
2022-05-13 18:25:17 +0600

    update

--- src/Component/Pages/Home/Home.js
+++ src/Component/Pages/Home/Home.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>home</h1>
-        </div>
+      <div>
+        <h1>home</h1>
+        <button class="btn">Button</button>
+      </div>
     );
 };
 
